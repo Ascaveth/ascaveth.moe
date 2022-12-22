c8d064b6893e9cca5213fbce616873e1f69be6f9
Ascaveth <angyevance@gmail.com>
2022-12-22 23:02:52 +0700

    Creating the base color pallete for the design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import Socials from './Socials';
+import Logo from '../img/header/logo.svg';
+import MobileNav from './MobileNav';
+
+import { Link } from 'react-router-dom';
+
 const Header = () => {
-  return <div>Header</div>;
+  return <Header className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
+    <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between'>
+      <Link to={'/'}>
+        <img src={Logo} alt=''/>
+      </Link>
+      <nav className='hidden lg:flex'>
+        <Link to={'/'}>Home</Link>
+      </nav>
+    </div>
+  </Header>;
 };
 
 export default Header;
